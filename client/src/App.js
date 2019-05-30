0e548ea9596c124892897cf51b531d53c221439e
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-05-30 17:02:19 -0500

    Inialized the frontend, added bootstrap and some styling

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import './App.css';
+import { Card, CardTitle, Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <Card>
+          <CardTitle >
+            Hello How To
+          </CardTitle>
+          <Button>
+            Get Started
+          </Button>
+        </Card>
+      </header>
+    </div>
+  );
+}
+
+export default App;

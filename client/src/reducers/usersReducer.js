f8b52c55b94453f22c50d4ba61461614e5d605b3
AJLambda <apj2277@gmail.com>
2019-06-06 17:42:46 -0700

    added login reducers

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -1,66 +1,92 @@
-import { 
-    USER_FETCH, USER_SUCCESS, USER_FAIL, 
-    REGISTER_START, REGISTER_SUCCESS, REGISTER_FAILURE,
+import {
+  USER_FETCH,
+  USER_SUCCESS,
+  USER_FAIL,
+  REGISTER_START,
+  REGISTER_SUCCESS,
+  REGISTER_FAILURE,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE
+} from "../actions";
 
-} from '../actions';
-
-const initialState =
-{
-    user: { message: 'wow' },
-    fetching: false,
-    adding: false,
-    error: null,
-    isRegistered: false,
-    isLoggedIn: false,
+const initialState = {
+  user: { message: "wow" },
+  fetching: false,
+  adding: false,
+  error: null,
+  isRegistered: false,
+  isLoggedIn: false,
+  token: ""
 };
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case USER_FETCH:
+      return {
+        ...state,
+        fetching: true
+      };
+    case USER_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        fetching: false,
+        user: action.payload
+      };
+    case USER_FAIL:
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload
+      };
 
-    switch (action.type) {
+    case REGISTER_START:
+      return {
+        ...state,
+        isRegistered: true,
+        error: null
+      };
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        isRegistered: false,
+        isLoggedIn: true,
+        user_id: action.payload.id,
+        error: null
+      };
+    case REGISTER_FAILURE:
+      return {
+        ...state,
+        isRegistered: false,
+        error: action.payload
+      };
 
-        case USER_FETCH:
-            return {
-                ...state,
-                fetching: true
-            }
-        case USER_SUCCESS:
-            return {
-                ...state,
-                error: null,
-                fetching: false,
-                user: action.payload
-            }
-        case USER_FAIL:
-            return {
-                ...state,
-                fetching: false,
-                error: action.payload
-            }
+    case LOGIN_START:
+      return {
+        ...state,
+        error: null
+      };
 
-        case REGISTER_START:
-            return {
-                ...state,
-                isRegistered: true,
-                error: null,
-            }
-        case REGISTER_SUCCESS:
-            return {
-                ...state,
-                isRegistered: false,
-                isLoggedIn: true,
-                user_id: action.payload.id,
-                error: null
-            }
-        case REGISTER_FAILURE:
-            return {
-                ...state,
-                isRegistered: false,
-                error: action.payload
-            }
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        token: action.payload.token,
+        user_id: action.payload.id,
+        isLoggedIn: true,
+        isRegistered: true
+      };
 
-        default:
-            return state;
-    }
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isLoggedIn: false,
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;

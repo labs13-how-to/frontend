f5c07ac656a48ce57cc2efba2bac6c09e29063a7
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-06-11 12:06:30 -0500

    CreateStep edited to submit properly

--- client/src/components/posts/CreatePostStep.js
+++ client/src/components/posts/CreatePostStep.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { addStep } from '../../actions/steps-tagsActions';
+import { getPost } from '../../actions/index';
 
 class CreateStepForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            post_id: 0,
-            step_num: 0,
+            post_id: this.props.match.params.id,
+            step_num: 1,
             title: '',
             instruction: '',
             img_url: '',
             vid_url: ''
         };
+    }
+
+    componentDidMount() {
+        this.props.getPost(this.state.post_id)
+        console.log(this.props.currPost)
     }
 
 
@@ -26,77 +32,84 @@ class CreateStepForm extends React.Component {
     handleSumbit = async e => {
         console.log(this.state);
         e.preventDefault();
-        await this.props.addStep(this.state);
+        await this.props.addStep(this.state.post_id, this.state);
 
         this.setState({
-            step_num: 0,
+            step_num: this.state.step_num + 1,
             title: '',
             instruction: '',
             img_url: '',
             vid_url: ''
         })
-        // setTimeout(() => this.props.history.push(`/posts/${this.props.id}`), 300);
+
+        console.log(this.props.currPost)
+        setTimeout(() => this.props.getPost(this.state.post_id), 300);
     }
 
     render() {
         return (
-            <Form onSubmit={this.handleSumbit}>
-                <FormGroup>
-                    <Label>Title</Label>
-                    <Input
-                        onChange={this.handleChange}
-                        placeholder='title'
-                        value={this.state.title}
-                        name='title'
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label>instruction</Label>
-                    <Input
-                        type="textarea"
-                        name='instruction'
-                        onChange={this.handleChange}
-                        value={this.state.instruction}
-                        placeholder='content'></Input>
+            <>
+                <Form onSubmit={this.handleSumbit}>
+                    <FormGroup>
+                        <Label>Title</Label>
+                        <Input
+                            onChange={this.handleChange}
+                            placeholder='title'
+                            value={this.state.title}
+                            name='title'
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>instruction</Label>
+                        <Input
+                            type="textarea"
+                            name='instruction'
+                            onChange={this.handleChange}
+                            value={this.state.instruction}
+                            placeholder='content'></Input>
 
-                </FormGroup>
-                <FormGroup>
-                    <Label>image(optional)</Label>
-                    <Input
-                        onChange={this.handleChange}
-                        placeholder='img_url'
-                        value={this.state.img_url}
-                        name='img_url'
-                    />
-                </FormGroup>
-
-
-                <FormGroup>
-                    <Label>Video(optional)</Label>
-                    <Input
-                        onChange={this.handleChange}
-                        placeholder='vid_url'
-                        value={this.state.vid_url}
-                        name='vid_url'
-                    />
-                </FormGroup>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>image(optional)</Label>
+                        <Input
+                            onChange={this.handleChange}
+                            placeholder='img_url'
+                            value={this.state.img_url}
+                            name='img_url'
+                        />
+                    </FormGroup>
 
 
-                <Button type='submit'>Save</Button>
-            </Form>
+                    <FormGroup>
+                        <Label>Video(optional)</Label>
+                        <Input
+                            onChange={this.handleChange}
+                            placeholder='vid_url'
+                            value={this.state.vid_url}
+                            name='vid_url'
+                        />
+                    </FormGroup>
+
+
+                    <Button type='submit'>Save</Button>
+                </Form>
+                <Button onClick={() => this.props.history.push(`/posts/${this.state.post_id}`)}>Done</Button>
+            </>
         )
     }
 }
 
 function mapStateToProps({ projectsReducer }) {
     return {
-        error: projectsReducer.error
+        error: projectsReducer.error,
+        currPost: projectsReducer.currPost
     }
 }
 
 export default connect(
     mapStateToProps,
     {
-        addStep
+        addStep,
+        getPost
     }
 )(CreateStepForm);

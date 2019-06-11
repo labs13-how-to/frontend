4a2fb9e6d5524e53affc2d33dfc705029013bb33
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-06-10 21:07:59 -0500

    post changed to route to steps form

--- client/src/components/posts/CreatePost.js
+++ client/src/components/posts/CreatePost.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Route } from "react-router-dom";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { addPost } from '../../actions';
 
@@ -28,8 +29,8 @@ class CreatePostForm extends React.Component {
     handleSumbit = async e => {
         console.log(this.state);
         e.preventDefault();
-        await this.props.addPost(this.state);
-
+        const newPost = await this.props.addPost(this.state);
+        // console.log(newPost)
         this.setState({
             title: '',
             img_url: '',
@@ -40,90 +41,98 @@ class CreatePostForm extends React.Component {
             supplies: '',
             created_by: 0,
         })
-        setTimeout(() => this.props.history.push('/'), 300);
+        setTimeout(() => this.props.history.push(`/forms/post/${newPost.id}/steps`), 300);
     }
 
     render() {
         return (
-            <Form onSubmit={this.handleSumbit}>
-                <FormGroup>
-                    <Label>Title</Label>
-                    <Input
-                        onChange={this.handleChange}
-                        placeholder='title'
-                        value={this.state.title}
-                        name='title'
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label>image</Label>
-                    <Input
-                        onChange={this.handleChange}
-                        placeholder='img_url'
-                        value={this.state.img_url}
-                        name='img_url'
-                    />
-                </FormGroup>
+            <>
+                <Form onSubmit={this.handleSumbit}>
+                    <FormGroup>
+                        <Label>Title</Label>
+                        <Input
+                            onChange={this.handleChange}
+                            placeholder='title'
+                            value={this.state.title}
+                            name='title'
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>image</Label>
+                        <Input
+                            onChange={this.handleChange}
+                            placeholder='img_url'
+                            value={this.state.img_url}
+                            name='img_url'
+                        />
+                    </FormGroup>
 
-                <FormGroup>
-                    <Label>description</Label>
-                    <Input
-                        type="textarea"
-                        name='description'
-                        onChange={this.handleChange}
-                        value={this.state.description}
-                        placeholder='content'></Input>
+                    <FormGroup>
+                        <Label>description</Label>
+                        <Input
+                            type="textarea"
+                            name='description'
+                            onChange={this.handleChange}
+                            value={this.state.description}
+                            placeholder='content'></Input>
 
-                </FormGroup>
-                <FormGroup>
-                    <Label>difficulty</Label>
-                    <Input
-                        onChange={this.handleChange}
-                        placeholder='difficulty'
-                        value={this.state.difficulty}
-                        name='difficulty'
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label>duration</Label>
-                    <Input
-                        onChange={this.handleChange}
-                        placeholder='duration'
-                        value={this.state.duration}
-                        name='duration'
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label>skills</Label>
-                    <Input
-                        onChange={this.handleChange}
-                        placeholder='skills'
-                        value={this.state.skills}
-                        name='skills'
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label>supplies</Label>
-                    <Input
-                        onChange={this.handleChange}
-                        placeholder='supplies'
-                        value={this.state.supplies}
-                        name='supplies'
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label></Label>
-                    <Input
-                        type="number"
-                        onChange={this.handleChange}
-                        placeholder='created_by'
-                        value={this.state.created_by}
-                        name='created_by'
-                    />
-                </FormGroup>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>difficulty</Label>
+                        <Input
+                            onChange={this.handleChange}
+                            placeholder='difficulty'
+                            value={this.state.difficulty}
+                            name='difficulty'
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>duration</Label>
+                        <Input
+                            onChange={this.handleChange}
+                            placeholder='duration'
+                            value={this.state.duration}
+                            name='duration'
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>skills</Label>
+                        <Input
+                            onChange={this.handleChange}
+                            placeholder='skills'
+                            value={this.state.skills}
+                            name='skills'
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>supplies</Label>
+                        <Input
+                            onChange={this.handleChange}
+                            placeholder='supplies'
+                            value={this.state.supplies}
+                            name='supplies'
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label></Label>
+                        <Input
+                            type="number"
+                            onChange={this.handleChange}
+                            placeholder='created_by'
+                            value={this.state.created_by}
+                            name='created_by'
+                        />
+                    </FormGroup>
 
-                <Button type='submit'>Save</Button>
-            </Form>
+                    <Button type='submit'>Save</Button>
+                </Form>
+                <Route path="/one" render={props => (
+                    <>
+                        <h1>New Route!!!</h1>
+                    </>
+                )}
+                />
+            </>
         )
     }
 }

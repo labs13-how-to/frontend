1d6ae79189bf3a8233da547447a122623d5dde89
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-06-11 19:27:13 -0400

    Changes UserPosts component to only display user specified in URL

--- client/src/components/users/UserPosts.js
+++ client/src/components/users/UserPosts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from "react-router";
 import { connect } from 'react-redux';
 import { Card, CardText, CardBody, CardHeader, CardImg, Button } from 'reactstrap';
 import { getUserPosts } from '../../actions';
@@ -6,15 +7,15 @@ import { getUserPosts } from '../../actions';
 class UserPosts extends React.Component {
 
     componentDidMount() {
-        this.props.getUserPosts();
+        this.props.getUserPosts(this.props.match.params.id);
     };
 
     render() {
         return(
             <div className='post-list'>
-                {this.props.posts.map(post => {
+                {this.props.posts.map((post, i) => {
                     return (
-                        <Card>
+                        <Card key={i}>
                             <CardImg src={post.img_url} alt="Card Image"/>
                             <CardHeader>{post.title}</CardHeader>
                             <CardBody>
@@ -32,14 +33,14 @@ class UserPosts extends React.Component {
     };
 };
 
-function mapStateToProps({ projectsReducer }) {
+function mapStateToProps({ projectsReducer, usersReducer }) {
     return {
-        posts: projectsReducer.posts
-        
+        posts: projectsReducer.posts,
+        user: usersReducer.user.id
     };
 };
 
 
 
 
-export default connect(mapStateToProps, { getUserPosts })(UserPosts);
+export default withRouter(connect(mapStateToProps, { getUserPosts })(UserPosts));

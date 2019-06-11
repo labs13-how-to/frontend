dacf2420184bebea86830db86afb1f0bb0e4f003
mrsteveson <patrick.steveson727@gmail.com>
2019-06-11 12:16:02 -0500

    initializing reviews and reviewForm

---
+++ client/src/components/reviews/Reviews.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { connect } from "react-redux";
+import { getReviews, getUsers } from "../../actions";
+import { Card, CardText, CardBody, CardHeader, CardImg } from "reactstrap";
+
+
+class Reviews extends React.Component {
+   
+    componentDidMount() {
+        this.props.getReviews(this.props.post_id);
+    };
+
+    render() {
+        return (
+            <div>
+                {this.props.reviews.map((review, index) => {
+                    // this.props.getUsers(review.user_id)
+                    // const currentUser = this.props.user.username
+                    return (
+                        <Card key={index}>
+                            <CardHeader>{review.username}</CardHeader>
+                            <CardBody>
+                                <CardText>{review.rating}</CardText>
+                                <CardText>{review.review}</CardText>
+                            </CardBody>
+                            {/* need something for up/down votes */}
+                        </Card>)
+                })}
+            </div>
+            
+        );
+    };
+};
+
+function mapStateToProps({ reviewsReducer, usersReducer }) {
+    return {
+        reviews: reviewsReducer.reviews,
+        error: reviewsReducer.error,
+        user: usersReducer.user
+    };
+};
+
+export default connect(mapStateToProps, {getReviews, getUsers})(Reviews);

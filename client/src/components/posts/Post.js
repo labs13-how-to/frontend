a3cf54afd9afb3efb362d9d36c890eba42f42ccd
mrsteveson <patrick.steveson727@gmail.com>
2019-06-11 12:14:50 -0500

    updated Post component to grab ID for reviews

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Card, CardText, CardBody, CardHeader, CardImg } from 'reactstrap';
+import Reviews from "../reviews/Reviews";
 
 
 class Post extends React.Component {
-
+    state={
+        id: Number(this.props.location.pathname.split('/')[2]),
+    }
     componentDidMount() {
-        const id = Number(this.props.location.pathname.split('/')[2]);
-        this.props.getPost(id)
+        this.props.getPost(this.state.id)
     }
 
     render() {
@@ -30,7 +32,9 @@ class Post extends React.Component {
                     <CardText>skills: {skills}</CardText>
                     <CardText>supplies: {supplies}</CardText>
                 </CardBody>
-
+                <Reviews 
+                    post_id={this.state.id}
+                />
             </Card>
         );
     }

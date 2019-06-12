7581e0313383dc0302cd0a796e11e8d3647d1f80
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-06-11 19:00:01 -0500

    Set up Post to display tags

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { Card, CardText, CardBody, CardHeader, CardImg } from 'reactstrap';
+import { connect } from 'react-redux';
+import {
+    Card, CardText, CardBody, CardHeader, CardImg,
+    FormGroup, Label, Input, DropdownToggle, DropdownMenu, DropdownItem, InputGroupButtonDropdown
+} from 'reactstrap';
+import { getTag } from '../../actions/steps-tagsActions';
+import { getPost } from '../../actions/index';
+
 
 
 class Post extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleDropDown = this.toggleDropDown.bind(this);
+        this.state = {
+            dropdownOpen: false
+        };
+    }
+
+    toggleDropDown() {
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+        });
+    }
 
     componentDidMount() {
         const id = Number(this.props.location.pathname.split('/')[2]);
         this.props.getPost(id)
+        this.props.getTag();
+    }
+    componentDidUpdate(prevProps, prevState) {
+
     }
 
     render() {
@@ -17,11 +42,34 @@ class Post extends React.Component {
             difficulty,
             skills,
             supplies,
-            duration
-        } = this.props.post
+            duration,
+            tags
+        } = this.props.currPost
         return (
             <Card className='post'>
                 <CardImg src={img_url} alt="Card image" />
+                <CardBody>Tags:</CardBody>
+                <div className='tag-section'>
+                    <p className='post-tags'>
+                        {tags && tags.map(tag => <span key={tag.id}>{tag.name}</span>)}
+                    </p>
+                    <InputGroupButtonDropdown addonType="append" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
+                        <DropdownToggle split outline />
+                        <DropdownMenu>
+                            <DropdownItem>
+                                <FormGroup>
+                                    <Label for="exampleSelectMulti">Select Tags</Label>
+                                    <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+                                        {this.props.allTags ? this.props.allTags.map(tag => <option key={tag.id}>{tag.name}</option>) : null}
+                                    </Input>
+                                </FormGroup>
+
+                            </DropdownItem>
+
+                        </DropdownMenu>
+                    </InputGroupButtonDropdown >
+                </div>
+
                 <CardHeader>{title}</CardHeader>
                 <CardBody>
                     <CardText>{description}</CardText>
@@ -35,5 +83,18 @@ class Post extends React.Component {
         );
     }
 };
+function mapStateToProps({ projectsReducer }) {
+    return {
+        error: projectsReducer.error,
+        currPost: projectsReducer.currPost,
+        allTags: projectsReducer.allTags
+    }
+}
 
-export default Post;
+export default connect(
+    mapStateToProps,
+    {
+        getTag,
+        getPost
+    }
+)(Post);

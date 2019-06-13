b3f0eb7008ae12553390368cff5ae31544d6d256
AJLambda <apj2277@gmail.com>
2019-06-13 13:12:35 -0700

    updates app for responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Route, NavLink } from "react-router-dom";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Route } from "react-router-dom";
 import { withRouter } from "react-router";
 import Home from "./components/Home";
 import Nav from "./components/navbar/Nav";
@@ -8,19 +8,22 @@ import Users from "./components/users/Users";
 // import Register from "./components/users/Register";
 import Post from "./components/posts/Post";
 import CreatePost from "./components/posts/CreatePost.js";
-import { Button } from "reactstrap";
 import SearchResults from "./components/posts/SearchResults";
 import CreateStep from "./components/posts/CreatePostStep";
 import { getTest, login } from "./actions";
 import queryString from "query-string";
 
-
 class App extends Component {
   componentDidMount() {
+    this.setState({
+      ...this.state
+    });
+
     var query = queryString.parse(this.props.location.search);
     console.log("query:", query);
-    if (query.token) {
+    if (query.token && query.user) {
       window.localStorage.setItem("jwt", query.token);
+      window.localStorage.setItem("user_id", query.user);
       this.props.history.push("/");
     }
 
@@ -32,56 +35,39 @@ class App extends Component {
     this.props.login();
   };
 
+  logOut = () => {
+    console.log("logout");
+    this.setState({
+      ...this.state,
+      token: null
+    });
+    localStorage.clear();
+    //localStorage.removeItem('token')
+    //props.history.push('/login)
+    window.location.assign("/");
+  };
+
   render() {
     console.log("PROPS", this.props);
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>{this.props.message}</h1>
-
-          <Nav />
-
-          <a href={`${process.env.REACT_APP_BE_URL}/auth/google`}>
-            <Button>Login with google</Button>
-          </a>
-
-          <NavLink to={"/user/:id"}>
-            <Button>Account</Button>
-          </NavLink>
-        </header>
         <div className="container">
-          <Route exact path='/' render={props => <Home {...props} />} />
+          <header className="App-header">
+            <Nav logOut={this.logOut} />
+          </header>
+
+          <Route exact path="/" render={props => <Home {...props} />} />
+          <Route path="/user/:id" render={props => <Users {...props} />} />
           <Route
-            path="/user/:id"
-            render={props => (
-              <Users
-                {...props}
-              />
-            )}
-          />
-          <Route path="/forms/post/create" render={props => (
-            <CreatePost
-              {...props}
-            />
-          )}
+            path="/forms/post/create"
+            render={props => <CreatePost {...props} />}
           />
 
-          {/* <Route path="/register" render={props => <Register {...props} />} /> */}
+          <Route path="/posts/:id" render={props => <Post {...props} />} />
 
           <Route
-            path="/posts/:id"
-            render={props => (
-              <Post
-                {...props}
-              />
-            )}
-          />
-
-          <Route path="/forms/post/:id/steps" render={props => (
-            <CreateStep
-              {...props}
-            />
-          )}
+            path="/forms/post/:id/steps"
+            render={props => <CreateStep {...props} />}
           />
 
           <Route

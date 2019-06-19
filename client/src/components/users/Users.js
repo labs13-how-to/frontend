955349e22a83672011f6fb5a5acc257b81f7f0e3
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-06-19 18:36:21 -0500

    changed what data is rendered and passed down

--- client/src/components/users/Users.js
+++ client/src/components/users/Users.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { getUsers } from "../../actions/index";
+import { getUsers, getPosts } from "../../actions/index";
 import UserPosts from './UserPosts';
 import {
     Button
@@ -9,6 +9,7 @@ import Favorites from "./FavoritePosts";
 
 class Users extends Component {
     componentDidMount() {
+        this.props.getPosts();
         const id = (this.props.location.pathname.split("/")[2]);
         this.props.getUsers(id);
         console.log(
@@ -18,27 +19,31 @@ class Users extends Component {
     }
 
     render() {
-        console.log("this.props.user:", this.props.user)
+        const userPosts = this.props.posts.filter((post) => post.created_by === this.props.match.params.id)
+        const posts_count = userPosts && userPosts.length
         return (
             <React.Fragment>
                 <h2>{this.props.user.username}'s Posts</h2>
+                <p>{posts_count}</p>
 
-                <UserPosts history={this.props.history} />
+                <UserPosts history={this.props.history} userPosts={userPosts} />
                 <Button onClick={() => this.props.history.push(`/user/${this.props.match.params.id}/favorites`)}>Favorites</Button>
             </React.Fragment>
         )
     }
 };
 
-function mapStateToProps({ usersReducer }) {
+function mapStateToProps({ usersReducer, projectsReducer }) {
     return {
-        user: usersReducer.user
+        user: usersReducer.user,
+        posts: projectsReducer.posts
     };
 }
 
 export default connect(
     mapStateToProps,
     {
-        getUsers
+        getUsers,
+        getPosts
     }
 )(Users);

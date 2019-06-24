8483910545bbba3844344cbc3a853d1caddbffe2
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-06-24 16:04:29 -0500

    Protected route added

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import './Protected.scss';
+
+const PostList = (props) => {
+
+
+    return (
+        <>
+            <h1>this is a restricted Route</h1>
+            {window.localStorage.getItem('user_id') && window.localStorage.getItem('jwt') ?
+                <div className='join-btn-div-mobile'>
+                    <Button className="join-btn join-btn-mobile" href={`/`}>Home</Button>
+                </div>
+                : <div className='join-btn-div-mobile'>
+                    <Button className="join-btn join-btn-mobile" href={`${process.env.REACT_APP_BE_URL}/auth/google`}>Join Now</Button>
+                </div>
+            }
+
+        </>
+    );
+
+};
+
+export default PostList;

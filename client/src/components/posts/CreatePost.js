f475cf503b69483c8f4c0c5c06e9e610642e5648
mrsteveson <patrick.steveson727@gmail.com>
2019-06-25 12:50:59 -0500

    style polish on forms done with designer

--- client/src/components/posts/CreatePost.js
+++ client/src/components/posts/CreatePost.js
@@ -27,67 +27,6 @@ import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 
 import ProtectedRoute from '../ProtectedRoute.js';
 
-// class CreatePostForm extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.toggleDropDown = this.toggleDropDown.bind(this);
-//         this.toggleDifficulty = this.toggleDifficulty.bind(this);
-//         this.state = {
-//             title: '',
-//             img_url: '',
-//             vid_url: '',
-//             description: '',
-//             difficulty: '',
-//             duration: '',
-//             skills: '',
-//             supplies: '',
-//             created_by: '',
-//             dropdownOpen: false,
-//             difficultyDropdown: false,
-//             tags: [],
-//             postImage: null,
-//             submit: false,
-//         };
-//     }
-
-//     componentDidMount() {
-//         this.hydrateStateWithLocalStorage();
-//         this.props.getTag();
-//     }
-//     componentDidUpdate(prevProps, prevState) {
-//         if (prevProps.submitRefresh !== this.props.submitRefresh) {
-//             this.handlePostSubmit()
-//         }
-
-//     }
-
-//     hydrateStateWithLocalStorage() {
-//         // if the key exists in localStorage
-//         if (!this.state.created_by) {
-//             // get the key's value from localStorage
-//             const id = localStorage.getItem('user_id');
-
-//             try {
-//                 this.setState({ created_by: `${id}` });
-//             } catch (e) {
-//                 // handle empty string
-//                 this.setState({ created_by: `${id}` });
-//             }
-//         }
-//     }
-
-//     //toggle functions
-//     toggleDropDown() {
-//         this.setState({
-//             dropdownOpen: !this.state.dropdownOpen
-//         });
-//     };
-//     toggleDifficulty() {
-//         this.setState({
-//             difficultyDropdown: !this.state.difficultyDropdown
-//         });
-//     };
-// Register FilePond the plugins
 registerPlugin(
     FilePondPluginFileEncode,
     FilePondPluginFileValidateType,
@@ -134,9 +73,7 @@ class CreatePostForm extends React.Component {
         if (!this.state.created_by) {
             // get the key's value from localStorage
             const id = localStorage.getItem("user_id");
-            console.log("ID:", id);
             try {
-                console.log("ID", id);
                 this.setState({ created_by: `${id}` });
             } catch (e) {
                 // handle empty string
@@ -170,23 +107,18 @@ class CreatePostForm extends React.Component {
             tag => e.target.value === tag.name.toLowerCase()
         );
         const newTag = { tag_id: tagId[0].id, name: e.target.value };
-        console.log("isTagged", isTagged);
-        console.log("newtag", newTag);
         if (!isTagged.length) {
             this.setState({ tags: [...this.state.tags, newTag] });
         } else {
             const filteredAry = this.state.tags.filter(
                 tag => tag.tag_id !== newTag.tag_id
             );
-            console.log(filteredAry);
             this.setState({ tags: filteredAry });
         }
     };
 
     handleSupplies = e => {
-        // console.log(e)
         if (e.key === 'Enter') {
-            // console.log(e.target.value)
             this.setState({ supplyList: [...this.state.supplyList, this.state.supplies] });
             this.setState({ supplies: '' });
         }
@@ -236,7 +168,6 @@ class CreatePostForm extends React.Component {
                 created_by: ""
             });
 
-            console.log(this.props.addId);
             setTimeout(() => {
                 this.state.tags.forEach(tag => {
                     const newTag = { tag_id: tag.tag_id, post_id: this.props.addId };
@@ -252,9 +183,6 @@ class CreatePostForm extends React.Component {
     }
 
     render() {
-        console.log("IMAGE", this.props.uploadedImage);
-        console.log("THIS STATE", this.state);
-
         return (
             <div className="pf-container">
                 <Form className="post-form" onSubmit={this.handleSubmit}>
@@ -271,49 +199,50 @@ class CreatePostForm extends React.Component {
                     <FormGroup className="pf-img">
                         <Label>Main Image</Label>
                         {/* <Input
-              type="file"
-              // name="img_url"
-              // id="img_url"
-              accept="image/png, image/jpeg"
-              onChange={this.handleImageChange}
-              disabled={this.state.disabled}
-            /> */}
-                    </FormGroup>
-                    <div className="filepond-container">
-                        <FilePond
-                            ref={ref => (this.pond = ref)}
-                            name="image"
-                            id="image"
-                            acceptedFileTypes={["image/png", "image/jpeg"]}
-                            disabled={this.state.disabled}
-                            allowMultiple={false}
-                            allowRevert={false}
-                            server={{
-                                // Sends image to be uploaded to cloudinary right after drag/dropped
-                                process: {
-                                    url: `${process.env.REACT_APP_BE_URL}/upload`,
-                                    onload: response => {
-                                        const json = JSON.parse(response);
-                                        console.log(json);
-                                        this.setState({
-                                            img_url: json.img_url.img_url
-                                        });
+                                type="file"
+                                // name="img_url"
+                                // id="img_url"
+                                accept="image/png, image/jpeg"
+                                onChange={this.handleImageChange}
+                                disabled={this.state.disabled}
+                            /> */}
+                        <div className="filepond-container">
+                            <FilePond
+                                ref={ref => (this.pond = ref)}
+                                name="image"
+                                id="image"
+                                acceptedFileTypes={["image/png", "image/jpeg"]}
+                                disabled={this.state.disabled}
+                                allowMultiple={false}
+                                allowRevert={false}
+                                server={{
+                                    // Sends image to be uploaded to cloudinary right after drag/dropped
+                                    process: {
+                                        url: `${process.env.REACT_APP_BE_URL}/upload`,
+                                        onload: response => {
+                                            const json = JSON.parse(response);
+                                            console.log(json);
+                                            this.setState({
+                                                img_url: json.img_url.img_url
+                                            });
+                                        }
                                     }
-                                }
-                            }}
-                            oninit={() => this.handleInit()}
-                            // allowFileEncode={true}
-                            onupdatefiles={fileItems => {
-                                console.log("FILE ITEMS", fileItems);
-                                // Set current file object to this.state
-                                this.setState({
-                                    postImage: fileItems[0].file
-                                });
-                            }}
-                        />
-                    </div>
+                                }}
+                                oninit={() => this.handleInit()}
+                                // allowFileEncode={true}
+                                onupdatefiles={fileItems => {
+                                    console.log("FILE ITEMS", fileItems);
+                                    // Set current file object to this.state
+                                    this.setState({
+                                        postImage: fileItems[0].file
+                                    });
+                                }}
+                            />
+                        </div>
+                    </FormGroup>
+                    
                     <FormGroup className="pf-img">
-                        <Label>Youtube Video (optional)</Label>
+                        <Label>Youtube Video <span className="video-span">(Optional)</span></Label>
                         <Input
                             onChange={this.handleChange}
                             placeholder="Include a YouTube video here"
@@ -321,53 +250,50 @@ class CreatePostForm extends React.Component {
                             name="vid_url"
                         />
                     </FormGroup>
-                    <p>
-                        Category{" "}
-                        <span className="category-span">
-                            (click the same category to unselect)
-            </span>
-                    </p>
-                    <div className="tag-section">
-                        <p className="post-tags">
-                            {this.state.tags && this.state.tags.map((tag, index) => {
-                                let currTag = tag.name.split('');
-                                currTag[0] = currTag[0].toUpperCase();
-                                currTag = currTag.join('')
-                                return <span key={index}>{currTag}<span id={'tag-delete'} onClick={() => this.handleTagsChange({ target: { value: tag.name } })} >🗴</span></span>
-                            })}
-                        </p>
-                        <InputGroupButtonDropdown
-                            addonType="append"
-                            isOpen={this.state.dropdownOpen}
-                            toggle={this.toggleDropDown}
-                        >
-                            <DropdownToggle split outline>
-                                {"Add/Delete Tags\xa0"}{" "}
-                            </DropdownToggle>
-                            <DropdownMenu>
-                                <DropdownItem>
-                                    <FormGroup>
-                                        <Label for="exampleSelectMulti">Select Tags</Label>
-                                        <Input
-                                            onChange={this.handleTagsChange}
-                                            type="select"
-                                            name="selectMulti"
-                                            id="exampleSelectMulti"
-                                            multiple
-                                        >
-                                            {this.props.allTags
-                                                ? this.props.allTags.map(tag => (
-                                                    <option key={tag.id} value={tag.name.toLowerCase()}>
-                                                        {tag.name}
-                                                    </option>
-                                                ))
-                                                : null}
-                                        </Input>
-                                    </FormGroup>
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </InputGroupButtonDropdown>
-                    </div>
+                    <FormGroup>
+                        <p>Category{" "}</p>
+                        <div className="tag-section">
+                            <p className="post-tags form-tags">
+                                {this.state.tags && this.state.tags.map((tag, index) => {
+                                    let currTag = tag.name.split('');
+                                    currTag[0] = currTag[0].toUpperCase();
+                                    currTag = currTag.join('')
+                                    return <span key={index}>{currTag}<span id={'tag-delete'} onClick={() => this.handleTagsChange({ target: { value: tag.name } })} >🗴</span></span>
+                                })}
+                            </p>
+                            <InputGroupButtonDropdown
+                                addonType="append"
+                                isOpen={this.state.dropdownOpen}
+                                toggle={this.toggleDropDown}
+                            >
+                                <DropdownToggle split outline>
+                                    {"Select Categories\xa0"}{" "}
+                                </DropdownToggle>
+                                <DropdownMenu>
+                                    <DropdownItem>
+                                        <FormGroup>
+                                            {/* <Label for="exampleSelectMulti">Select Tags</Label> */}
+                                            <Input
+                                                onChange={this.handleTagsChange}
+                                                type="select"
+                                                name="selectMulti"
+                                                id="exampleSelectMulti"
+                                                multiple
+                                            >
+                                                {this.props.allTags
+                                                    ? this.props.allTags.map(tag => (
+                                                        <option key={tag.id} value={tag.name.toLowerCase()}>
+                                                            {tag.name}
+                                                        </option>
+                                                    ))
+                                                    : null}
+                                            </Input>
+                                        </FormGroup>
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </InputGroupButtonDropdown>
+                        </div>
+                    </FormGroup>
 
                     <FormGroup className="pf-description">
                         <Label>Introduction</Label>
@@ -403,11 +329,11 @@ class CreatePostForm extends React.Component {
                                             id="exampleSelectMulti"
                                             multiple
                                         >
+                                            <option value={`Very Easy`}>Very Easy</option>
                                             <option value={`Easy`}>Easy</option>
-                                            <option value={`Intermediate`}>Intermediate</option>
+                                            <option value={`Moderate`}>Moderate</option>
                                             <option value={`Hard`}>Hard</option>
                                             <option value={`Very Hard`}>Very Hard</option>
-                                            <option value={`Impossible`}>Impossible</option>
                                         </Input>
                                     </FormGroup>
                                 </DropdownItem>
@@ -418,7 +344,7 @@ class CreatePostForm extends React.Component {
                         <Label>Duration</Label>
                         <Input
                             onChange={this.handleChange}
-                            placeholder="Estimated Time to Complete"
+                            placeholder="How long did it take to complete this project?"
                             value={this.state.duration}
                             name="duration"
                         />
@@ -427,7 +353,7 @@ class CreatePostForm extends React.Component {
                         <Label>Prerequisite Skills</Label>
                         <Input
                             onChange={this.handleChange}
-                            placeholder="Skills Needed for this Project"
+                            placeholder="Are there any skills needed to complete this project?"
                             value={this.state.skills}
                             name="skills"
                         />
@@ -435,7 +361,7 @@ class CreatePostForm extends React.Component {
                     <FormGroup className="pf-supplies">
                         <Label>Tools/Supplies{" "}
                             <span className="category-span">
-                                (Type your Item and press Enter)
+                                (Type your item and press Enter)
                             </span>
                         </Label>
                         <p className='post-tags'>
@@ -454,7 +380,7 @@ class CreatePostForm extends React.Component {
                         <Input
                             onKeyDown={this.handleSupplies}
                             onChange={this.handleChange}
-                            placeholder='Add an item'
+                            placeholder='What tools or supplies did you use for this project?'
                             value={this.state.supplies}
                             name='supplies'
                         />
